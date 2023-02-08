f52721b1119a968265ef37ea49ce59a91e97a7a2
Miriam <47831805+miriamkw@users.noreply.github.com>
2023-02-08 12:04:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 project_name = "Tidepool Data Science Project"
-version = "0.1.0"
+version_string = "v0.1.0"
 author = "Your Name"
 author_email = "YourName@tidepool.org"
 package_name = "tidepool_data_science_project"  # this is the thing you actually import
