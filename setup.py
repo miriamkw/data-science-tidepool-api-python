99c1ab3fbe9cb5c6ea223f785f595590e61fe285
Miriam K. Wolff <miriamkwolff@hotmail.com>
2023-10-15 12:09:42 +0200

    Renamed folder to simplify pip installs

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 project_name = "tidepool_data_science_project"
-version = "0.0.1"
+version = "0.0.3"
 author = "Your Name"
 author_email = "YourName@tidepool.org"
 package_name = "tidepool_data_science_project"  # this is the thing you actually import
@@ -15,8 +15,8 @@ setup(
     author=author,
     author_email=author_email,
     description="Python repository to facilitate using the Tidepool API (forked). ",
-    packages=[package_name],  # add subpackages too
-    package_dir={package_name: 'data_science_tidepool_api_python'},
+    packages=find_packages(),  # add subpackages too
+    package_dir={package_name: package_name},
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/miriamkw/data-science-tidepool-api-python",
